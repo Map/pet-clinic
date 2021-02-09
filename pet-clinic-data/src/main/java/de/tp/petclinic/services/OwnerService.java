00db6dccf76649c9381598f3024dd4e126c8560a
Map <t.petrik@gmx.net>
2021-02-09 11:00:32 +0100

    Implement interfaces for services. closes #5

---
+++ pet-clinic-data/src/main/java/de/tp/petclinic/services/OwnerService.java
@@ -0,0 +1,15 @@
+package de.tp.petclinic.services;
+
+import de.tp.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+    Owner findById(long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
