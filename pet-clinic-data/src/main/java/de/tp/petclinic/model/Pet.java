1ed88f421cdca107c1a51b7f0cc60b8ee2e614f5
Map <t.petrik@gmx.net>
2021-02-09 11:08:14 +0100

    adding base entity. closes #10

--- pet-clinic-data/src/main/java/de/tp/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/de/tp/petclinic/model/Pet.java
@@ -2,7 +2,7 @@ package de.tp.petclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType petType;
 
--- pet-clinic-data/src/main/java/de/tp/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/de/tp/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package de.tp.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
