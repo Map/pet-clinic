bb01ee2b968ba0a5581aeed23ee35c7b63bf9ee0
Map <t.petrik@gmx.net>
2021-02-09 20:19:20 +0100

    adding vets page and contoller. closes #7

---
+++ pet-clinic-web/src/main/java/de/tp/petclinic/controllers/VetController.java
@@ -0,0 +1,14 @@
+package de.tp.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping("/vets")
+    public String listVets(){
+
+        return "vets/index";
+    }
+}
