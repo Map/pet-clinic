a2a716f511963e75fb75efd0c7e5c7bf12c72a29
Map <t.petrik@gmx.net>
2021-02-10 20:02:31 +0100

    specialties added  #27

---
+++ pet-clinic-data/src/main/java/de/tp/petclinic/model/Specialty.java
@@ -0,0 +1,13 @@
+package de.tp.petclinic.model;
+
+public class Specialty extends BaseEntity{
+    private String desription;
+
+    public String getDesription() {
+        return desription;
+    }
+
+    public void setDesription(String desription) {
+        this.desription = desription;
+    }
+}
