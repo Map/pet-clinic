6ceaa157b1f5ec5957ea517d7bfc07e4a64465c1
Map <t.petrik@gmx.net>
2021-02-11 21:54:20 +0100

    closes #39

---
+++ pet-clinic-data/src/main/java/de/tp/petclinic/repositories/PetRepository.java
@@ -0,0 +1,10 @@
+package de.tp.petclinic.repositories;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by jt on 8/5/18.
+ */
+public interface PetRepository extends CrudRepository<Pet, Long> {
+}
---
+++ pet-clinic-data/src/main/java/de/tp/petclinic/repositories/VisitRepository.java
@@ -0,0 +1,10 @@
+package de.tp.petclinic.repositories;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by jt on 8/5/18.
+ */
+public interface VisitRepository extends CrudRepository<Visit, Long> {
+}
